4824f13e65dacc7c53dd5f6909b145fa31d4bbb5
tenpigs267 <126336487+tenpigs267@users.noreply.github.com>
2025-04-06 18:16:26 +0200

    initial

---
+++ src/main/java/fr/spinget/mcp_diagram/McpDiagramApplication.java
@@ -0,0 +1,13 @@
+package fr.spinget.mcp_diagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpDiagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpDiagramApplication.class, args);
+	}
+
+}
